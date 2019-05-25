84e7036d70e016244b08aa94185be64409768c0f
emreucar41 <42539421+emreucar41@users.noreply.github.com>
2019-05-25 23:10:11 +0300

    Update kız.js

--- komutlar/kız.js
+++ komutlar/kız.js
@@ -7,27 +7,22 @@ module.exports.run = async (bot, message, args) => {
     let member = message;
     var kayıt = bot.channels.get("579036622310342714")
 
-    if (!args[0]) return message.channel.send("Lütfen <!kız isim yaş> Olarak Yazınız!")
-    if (!args[1]) return message.channel.send("Lütfen <!kız isim yaş> Olarak Yazınız!")
-    if (args[1] < 7) return message.channel.send("Trolleme!")
-    if (args[1] > 30) return message.channel.send("Trolleme!")
-    if (args[2]) return message.channel.send("Trolleme!")
-
     var role = member.guild.roles.find("name", "Lütfen Kayıt Olunuz !");
     message.member.removeRole(role)
 
-    var role2 = member.guild.roles.find("name", "🌸 | Ladies");
+    var role2 = member.guild.roles.find("name", "Teyitsiz");
     message.member.addRole(role2);
 
-    message.member.setNickname(`∞ ${args[0]} [${args[1]}]`)
+    message.member.setNickname(`∞ ${message.author.username}`)
 
     message.reply("Tebrikler Başarıyla Kayıt Oldun!").then(msg => msg.delete(5000));
+
 
 
 }
 
 module.exports.help = {
-    name: "kız",
+    name: "erkek",
     decription: "Register Server And Join The Voice Channel vs.",
     usage: "?kayıt",
     accessableby: "Members",
